a2e4e7545d48596e8c9fbf19f8e1b2e290787be4
tindrajargen <tindra@jargenstedt.se>
2024-10-12 12:52:52 +0200

    removed commented out code

--- homeassistant/components/assist_pipeline/vad.py
+++ homeassistant/components/assist_pipeline/vad.py
@@ -144,43 +144,12 @@ class VoiceCommandSegmenter:
 
         if not self.in_command:
             self._handle_speech_detection(chunk_seconds, is_speech)
-            # if is_speech:
-            #     self._reset_seconds_left = self.reset_seconds
-            #     self._speech_seconds_left -= chunk_seconds
-            #     if self._speech_seconds_left <= 0:
-            #         # Inside voice command
-            #         self.in_command = True
-            #         self._command_seconds_left = (
-            #             self.command_seconds - self.speech_seconds
-            #         )
-            #         self._silence_seconds_left = self.silence_seconds
-            #         _LOGGER.debug("Voice command started")
-            # else:
-            #     # Reset if enough silence
-            #     self._reset_seconds_left -= chunk_seconds
-            #     if self._reset_seconds_left <= 0:
-            #         self._speech_seconds_left = self.speech_seconds
-            #         self._reset_seconds_left = self.reset_seconds
+
         elif not is_speech:
             return self._handle_silence_in_command(chunk_seconds)
-            # # Silence in command
-            # self._reset_seconds_left = self.reset_seconds
-            # self._silence_seconds_left -= chunk_seconds
-            # self._command_seconds_left -= chunk_seconds
-            # if (self._silence_seconds_left <= 0) and (self._command_seconds_left <= 0):
-            #     # Command finished successfully
-            #     self.reset()
-            #     _LOGGER.debug("Voice command finished")
-            #     return False
+
         else:
             self._handle_active_speech_in_command(chunk_seconds)
-            # # Speech in command.
-            # # Reset silence counter if enough speech.
-            # self._reset_seconds_left -= chunk_seconds
-            # self._command_seconds_left -= chunk_seconds
-            # if self._reset_seconds_left <= 0:
-            #     self._silence_seconds_left = self.silence_seconds
-            #     self._reset_seconds_left = self.reset_seconds
 
         return True
 
